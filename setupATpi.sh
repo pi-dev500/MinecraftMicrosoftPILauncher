c1dfdba6de0465f55da816fb765104d8c99a4aad
pi-dev500 <77228126+pi-dev500@users.noreply.github.com>
2021-03-29 13:40:32 +0200

    update splash screen installer

--- setupATpi.sh
+++ setupATpi.sh
@@ -160,7 +160,7 @@ fi
 rm -f launcher.jar
 echo "Downloading launcher..."
 wget -q --show-progress https://atlauncher.com/download/jar --output-document launcher.jar || error "failed to download \"launcher.jar\""
-mkdir -p launcher && cd launcher && mkdir -p ./assets/image && wget -q https://raw.githubusercontent.com/pi-dev500/MinecraftMicrosoftPILauncher/main/SplashScreen.png && mv SplashScreen.png assets/image/SplashScreen.png && zip -rm ../launcher.jar * >/dev/null && cd .. && rm -rf launcher
+mkdir -p launcher && cd launcher && mkdir -p ./assets/image && wget -q https://raw.githubusercontent.com/pi-dev500/MinecraftMicrosoftPILauncher/main/SplashScreen.png && mv SplashScreen.png assets/image/splash-screen.png && zip -rm ../launcher.jar * >/dev/null && cd .. && rm -rf launcher
 cd $DIR
 echo "Done!"
 # download lwjgl3arm*
